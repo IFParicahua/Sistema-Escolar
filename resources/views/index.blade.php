d04123f7285ce16078d91d561e67927bf6ca753c
IFParicahua <itati.faviola.paricahua@gmail.com>
2019-07-14 02:15:42 -0400

    Inicio vista

---
+++ resources/views/index.blade.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="{{ asset('css/fondo.css') }}" />
+    <link rel="stylesheet" href="{{ asset('boostrap/css/bootstrap.min.css') }}" />
+    <link rel="stylesheet" href="{{ asset('boostrap/js/bootstrap.min.js') }}" />
+
+    <title>School</title>
+</head>
+<body >
+<!-- col-lg-6 col-md-6 col-sm-6 col-xs-6 -->
+<div class="bloque">
+        <table class="col-12" style="background-color: #5a80fb;text-align:center;">
+            <thead>
+                <tr class="estilo">
+                    <th ><a href="http://www.google.com" ><img class="icono" src="iconos/businessman.svg"/></a></th>
+                    <th ><a href="http://www.google.com" ><img class="icono" src="iconos/businesswoman.svg"/></a></th>
+                    <th ><a href="http://www.google.com" ><img class="icono" src="iconos/man-13.svg"/></a></th>
+                    <th ><a href="http://www.google.com" ><img class="icono" src="iconos/woman-2.svg"/></a></th>
+                    <th ><a href="http://www.google.com" ><img class="icono" src="iconos/man-32.svg"/></a></th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr class="let">
+                    <th><label >Administrador</label></th>
+                    <th><label >Contador</label></th>
+                    <th><label >Regente</label></th>
+                    <th><label >Profesor</label></th>
+                    <th><label >Padre</label></th>
+                </tr>
+            </tbody>
+        </table>  
+</div>
+    
+    
+</body>
+<!-- <footer>
+    <div>Icons made by 
+        <a href="https://www.flaticon.com/authors/eucalyp" title="Eucalyp">Eucalyp</a> from 
+        <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a> is licensed by 
+        <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a>
+    </div>
+</footer> -->
+</html>
